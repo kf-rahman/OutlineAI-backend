7e9d5ccde5c76f19cfc521a9db9ecdc0b7137428
kazirahman <kazif2122r@gmail.com>
2024-09-19 22:43:58 -0400

    some reason auth function got deleted

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ const generationConfig = {
 };
 
 // OAuth authentication routes
+app.get('/auth', (req, res) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+    });
+    res.redirect(authUrl);
+});
 app.get('/', async (req, res) => {
    res.send('hello') ;
 });
